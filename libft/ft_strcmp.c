2656b94c76cc0de67f450e89ac79363123c4bddf
Andrei serban ZAHA <azaha@e1p8.cluj.42.fr>
2015-11-10 17:01:13 +0100

    asda

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: azaha <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/24 20:12:51 by azaha             #+#    #+#             */
-/*   Updated: 2015/10/27 09:42:34 by azaha            ###   ########.fr       */
+/*   Updated: 2015/11/10 16:57:13 by azaha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	while (*(unsigned char*)s1 || *(unsigned char*)s2)
 	{
+		if (*(unsigned char*)s1 != *(unsigned char*)s2)
+			return (*(unsigned char*)s1 - *(unsigned char*)s2);
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return (0);
 }
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: azaha <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/24 20:25:43 by azaha             #+#    #+#             */
-/*   Updated: 2015/11/08 14:22:17 by azaha            ###   ########.fr       */
+/*   Updated: 2015/11/10 16:59:35 by azaha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,11 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	index = 0;
 	while ((*s1 || *s2) && index < n)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		else
-		{
-			s1++;
-			s2++;
-			index++;
-		}
+		if (*(unsigned char*)s1 != *(unsigned char*)s2)
+			return (*(unsigned char*)s1 - *(unsigned char*)s2);
+		s1++;
+		s2++;
+		index++;
 	}
 	return (0);
 }
