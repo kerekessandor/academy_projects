d29bd7e7978f513e2bfcce934476e83f9063d29b
Andrei serban ZAHA <azaha@e1p78.cluj.42.fr>
2015-11-13 20:25:49 +0100

    done

--- workshop_code_best_practices/best_code.c
+++ workshop_code_best_practices/best_code.c
@@ -6,7 +6,7 @@
 /*   By: azaha <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/13 14:23:38 by azaha             #+#    #+#             */
-/*   Updated: 2015/11/13 19:02:17 by azaha            ###   ########.fr       */
+/*   Updated: 2015/11/13 19:55:27 by azaha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,14 @@ void	print_students(student student)
 {
 	if (strcmp(student.county, "Cluj") == 0 && atoi(student.grade) >= 8.00)
 		printf("\033[32;1m%s %s has grade bigger than or equal to 8.\033[0m\n", student.first_name, student.last_name);
+	else
+		printf("\033[34;1m%s %s does not have grade bigger than 8.\033[0m\n", student.first_name, student.last_name);
 }
 
 void	fill_columns(char *to_complete, char *buff, int word_index, int buff_index)
 {
+	while (buff[buff_index] == ' ' || buff[buff_index] == '\t')
+		buff_index++;
 	to_complete[word_index] = buff[buff_index];
 	if (buff[buff_index + 1] == ';' || buff[buff_index + 1] == 0)
 		to_complete[word_index + 1] = '\0';
@@ -53,14 +57,18 @@ int		is_number(int n)
 int		check_first_name(student student, int *valid_student)
 {
 	int index = 0;
+	int valid = 1;
 
 	while (student.first_name[index])
 	{
 		if (is_alpha(student.first_name[index]) == 0)
+		{
 			*valid_student = 0;
+			valid = 0;
+		}
 		index++;
 	}
-	if (*valid_student == 0)
+	if (valid == 0)
 		printf("\033[31;1m%s is not a valid first name.\033[0m\n", student.first_name);
 	return (*valid_student);
 }
@@ -68,14 +76,18 @@ int		check_first_name(student student, int *valid_student)
 int		check_last_name(student student, int *valid_student)
 {
 	int index = 0;
+	int valid = 1;
 
 	while (student.last_name[index])
 	{
 		if (is_alpha(student.last_name[index]) == 0)
+		{
 			*valid_student = 0;
+			valid = 0;
+		}
 		index++;
 	}
-	if (*valid_student == 0)
+	if (valid == 0)
 		printf("\033[31;1m%s is not a valid last name.\033[0m\n", student.last_name);
 	return (*valid_student);
 }
@@ -83,33 +95,44 @@ int		check_last_name(student student, int *valid_student)
 int		check_county(student student, int *valid_student)
 {
 	int index = 0;
+	int valid = 1;
 	
 	while (student.county[index])
 	{
 		if (is_alpha(student.county[index]) == 0)
+		{
+			valid = 0;
 			*valid_student = 0;
+		}
 		index++;
 	}
-	if (*valid_student == 0)
+	if (valid == 0)
 		printf("\033[31;1m%s is not a valid county.\033[0m\n", student.county);
 	return(*valid_student);
 
 }
 
-int		check_e_mail(char *e_mail, int *valid_student)
+int		check_e_mail(student student, int *valid_student)
 {
 	int	is_valid = 0;
+	int valid = 1;
+	int index = 0;
 
-	while (*e_mail)
+	while (student.e_mail[index])
 	{
-		if (*e_mail == '@')
+		if (student.e_mail[index] == '@')
 			is_valid++;
-		if (*e_mail == '.')
+		if (student.e_mail[index] == '.')
 			is_valid++;
-		e_mail++;
+		index++;
 	}
 	if(is_valid != 2)
+	{
 		*valid_student = 0;
+		valid = 0;
+	}
+	if (valid == 0)
+		printf("\033[31;1m%s is not a valid e-mail.\033[0m\n", student.e_mail);
 	return (*valid_student);
 }
 
@@ -117,18 +140,25 @@ int		check_grade(student student, int *valid_student)
 {
 	int points = 0;
 	int index = 0;
+	int valid = 1;
 
 	while (student.grade[index])
 	{
 		if (student.grade[index] == '.')
 			points++;
 		else if (is_number(student.grade[index]) == 0)
+		{
 			*valid_student = 0;
+			valid = 0;
+		}
 		index++;
 	}
 	if (points > 1)
+	{
 		*valid_student = 0;
-	if (*valid_student == 0)
+		valid = 0;
+	}
+	if (valid == 0)
 		printf("\033[31;1m%s %s does not have a valid grade.\033[0m\n", student.first_name, student.last_name);
 	return (*valid_student);
 }
@@ -173,7 +203,7 @@ void	go_through_file(FILE *fp)
 		}
 		check_first_name(student, &valid_student);
 		check_last_name(student, &valid_student);
-		check_e_mail(student.e_mail, &valid_student);
+		check_e_mail(student, &valid_student);
 		check_county(student, &valid_student);
 		check_grade(student, &valid_student);
 		if(valid_student == 1)
@@ -189,5 +219,3 @@ int		main(void)
 	go_through_file(fp);
 	return (0);
 }
-
-// \033[31;1m ... \033[0m
