a96e9a9a84b12aa529853e877569a66401d348fd
Andrei serban ZAHA <azaha@e1p78.cluj.42.fr>
2015-11-13 17:10:11 +0100

    almost

--- workshop_code_best_practices/best_code.c
+++ workshop_code_best_practices/best_code.c
@@ -6,7 +6,7 @@
 /*   By: azaha <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/13 14:23:38 by azaha             #+#    #+#             */
-/*   Updated: 2015/11/13 15:24:37 by azaha            ###   ########.fr       */
+/*   Updated: 2015/11/13 17:08:19 by azaha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,59 @@ void	print_students(student student)
 		printf("%s %s\n", student.first_name, student.last_name);
 }
 
-void	fill_columns(char *to_complete, char *buff, int word_index, int line_index)
+void	fill_columns(char *to_complete, char *buff, int word_index, int buff_index)
 {
-	to_complete[word_index] = buff[line_index];
-	if (buff[line_index + 1] == ';' || buff[line_index + 1] == 0)
+	to_complete[word_index] = buff[buff_index];
+	if (buff[buff_index + 1] == ';' || buff[buff_index + 1] == 0)
 		to_complete[word_index + 1] = '\0';
+}
+
+int		is_alpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+int		check_first_name(char *first_name, int *valid_student)
+{
+	while (*first_name)
+	{
+		if (is_alpha(*first_name) == 0)
+			*valid_student = 0;
+		first_name++;
+	}
+	return (*valid_student);
+}
+
+int		check_e_mail(char *e_mail, int *valid_student)
+{
+	int	is_valid = 0;
+
+	//printf("%s\n", e_mail);
+	while (*e_mail)
+	{
+		if (*e_mail == '@')
+			is_valid++;
+		if (*e_mail == '.')
+			is_valid++;
+		e_mail++;
+	}
+	if(is_valid != 2)
+		*valid_student = 0;
+	return (*valid_student);
+}
+
+int		check_last_name(char *last_name, int *valid_student)
+{
+	//printf("%s\n", last_name);
+	while (*last_name)
+	{
+		if (is_alpha(*last_name) == 0)
+			*valid_student = 0;
+		last_name++;
+	}
+	return (*valid_student);
 }
 
 void	go_through_file(FILE *fp)
@@ -42,6 +90,7 @@ void	go_through_file(FILE *fp)
 	int		buff_index;
 	int		word_index;
 	int		column;
+	int		valid_student;
 
 	while (fscanf(fp, "%s", buff) > 0)
 	{
@@ -49,6 +98,7 @@ void	go_through_file(FILE *fp)
 		buff_index = 0;
 		word_index = 0;
 		column = 0;
+		valid_student = 1;
 		while (buff[buff_index] != '\0')
 		{
 			if (buff[buff_index] == ';')
@@ -63,7 +113,9 @@ void	go_through_file(FILE *fp)
 				if (column == 1)
 					fill_columns (student.last_name, buff, word_index, buff_index);
 				if (column == 2)
+				{
 					fill_columns (student.e_mail, buff, word_index, buff_index);
+				}
 				if (column == 3)
 					fill_columns (student.grade, buff, word_index, buff_index);
 				if (column == 4)
@@ -72,7 +124,12 @@ void	go_through_file(FILE *fp)
 			}
 			buff_index++;
 		}
-		print_students(student);
+		//printf("%s\n", student.e_mail);
+		check_first_name(student.first_name, &valid_student);
+		check_last_name(student.last_name, &valid_student);
+		check_e_mail(student.e_mail, &valid_student);
+		if(valid_student == 1)
+			print_students(student);
 	}
 }
 
