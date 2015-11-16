a8299d136e55cbd41d875b56e4620b70149798fd
Andrei serban ZAHA <azaha@e1p2.cluj.42.fr>
2015-11-16 19:15:39 +0100

    asdas

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azaha <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/16 16:27:55 by azaha             #+#    #+#             */
+/*   Updated: 2015/11/16 16:35:38 by azaha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft.h"
+# define BUFF_SIZE 
+
+int		get_next_line(int const fd, char **line);
+
+#endif
